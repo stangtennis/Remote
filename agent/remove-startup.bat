eb4e7ffb2ad5f6c9685fb1ef798a1227b18b765e
stangtennis <hansemand@gmail.com>
2025-10-05 18:04:18 +0200

    Fix auto-elevation: Use VBScript method for reliable UAC elevation

--- agent/remove-startup.bat
+++ agent/remove-startup.bat
@@ -1,18 +1,23 @@
 @echo off
-echo ========================================
-echo Remote Desktop Agent - Remove Startup
-echo ========================================
-echo.
 
 :: Check for admin rights
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo ERROR: This script requires Administrator privileges.
-    echo Please right-click and select "Run as Administrator"
-    echo.
-    pause
-    exit /b 1
+    :: Request administrator privileges
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
 )
+
+:: Running as admin now
+cd /d "%~dp0"
+
+echo ========================================
+echo Remote Desktop Agent - Remove Startup
+echo ========================================
+echo.
 
 echo Removing startup task...
 schtasks /delete /tn "RemoteDesktopAgent" /f
