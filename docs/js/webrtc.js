4414b79ae50d83c86e251f24684ccf0f43f1058b
stangtennis <stangtennis@users.noreply.github.com>
2025-12-08 22:08:58 +0100

    Add free TURN servers for WebRTC NAT traversal

--- docs/js/webrtc.js
+++ docs/js/webrtc.js
@@ -59,7 +59,23 @@ async function initWebRTC(session) {
     const configuration = session.turn_config || {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        // Free TURN server from OpenRelay
+        {
+          urls: 'turn:openrelay.metered.ca:80',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        },
+        {
+          urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+          username: 'openrelayproject',
+          credential: 'openrelayproject'
+        }
       ]
     };
 
