64a4c854ce48c56a5332b3dcb7e4b53c504a25e5
stangtennis <stangtennis@users.noreply.github.com>
2025-12-12 23:52:41 +0100

    v2.56.8: More aggressive DXGI recovery

--- agent/internal/tray/tray.go
+++ agent/internal/tray/tray.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Version of the agent - update this with each release
-const Version = "v2.56.7"
+const Version = "v2.56.8"
 const BuildDate = "2025-12-12"
 const VersionString = Version + " (" + BuildDate + ")"
 
