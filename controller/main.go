052fd81141bc5642a6d3ef61c424b6b64eaf73b1
stangtennis <stangtennis@users.noreply.github.com>
2026-05-21 09:57:05 +0200

    v3.1.79: Controller H.264 bitrate fix release

--- controller/main.go
+++ controller/main.go
@@ -20,7 +20,7 @@ var assets embed.FS
 
 // Version information - injected at build time via -ldflags -X
 var (
-	Version     = "v3.1.78"
+	Version     = "v3.1.79"
 	BuildDate   = "2026-05-21"
 	VersionInfo = ""
 )
